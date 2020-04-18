6a58a2caf48bdfd371de37dc7dafabf077030143
Brian Berg <bberg11@gmail.com>
2020-04-18 13:09:24 -0400

    Finish out remaining Student resource actions

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -20,9 +20,27 @@ class StudentsController < ApplicationController
     @student = Student.new
   end
 
+  def edit
+    @student = Student.find(params[:id])
+  end
+
   def show
     @student = Student.find(params[:id])
   end
+
+  def update
+    @student = Student.find(params[:id])
+
+    if @student.update(student_params)
+      flash[:success] = "#{@student.name}'s profile has been updated"
+      redirect_to student_path(@student)
+    else
+      errors = @student.errors.full_messages.join(", and ")
+      flash[:error] = "There was an error updating your profile. <br/> #{errors}"
+      render "edit"
+    end
+  end
+
   private
 
   def student_params
