e631559fcac19cea9ccad733f3da71e8ba7a6a7c
Brian Berg <bberg11@gmail.com>
2020-04-18 12:15:06 -0400

    Handle Student creation

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,7 +3,28 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
+  def create
+    @student = Student.new(student_params)
+
+    if @student.save
+      flash[:success] = "Thanks for signing up, #{@student.name}"
+      redirect_to student_path(@student)
+    else
+      flash[:error] = "There was an error creating your profile"
+      render "new"
+    end
+  end
+
   def new
     @student = Student.new
+  end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+  private
+
+  def student_params
+    params.require(:student).permit(:name, :email, :password)
   end
 end
